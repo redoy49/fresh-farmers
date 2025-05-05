f2e1ddc2078e098bc77cc89b814f7956394ab0fa
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-05-05 19:11:33 +0600

    setup project firebase and react router

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKjieAihU8UBF8PesA82YmHSkqYGAQ_Ls",
+  authDomain: "subscription-box-df504.firebaseapp.com",
+  projectId: "subscription-box-df504",
+  storageBucket: "subscription-box-df504.firebasestorage.app",
+  messagingSenderId: "1037140498584",
+  appId: "1:1037140498584:web:7fc9703186f1524fdd18ed",
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/routes/router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword";
+import MyProfile from "../pages/MyProfile";
+import NotFound from "../pages/NotFound";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/forgot-password", element: <ForgotPassword /> },
+      {
+        path: "/profile",
+        element: <MyProfile />,
+      },
+      { path: "/*", element: <NotFound /> },
+    ],
+  },
+]);
+
+export default router;
